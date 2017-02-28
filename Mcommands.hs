9ffd24a813fc29719417dc103589bba043bd1df7
alexander31415926535 <alexander.kubelsky@gmail.com>
2017-02-28 20:26:15 +0100

    Added Mcommlist,Mcommands, updated Cabal file.

---
+++ Mcommands.hs
@@ -0,0 +1,15 @@
+#!/usr/bin/runghc 
+
+import Text.XML.HXT.Core
+
+prefix = "/usr/share/maxima/5.38.1/doc/html/maxima_" -- 89 --104
+
+fullindex = map (\x -> prefix ++ show x ++ ".html") [89 .. 104]
+
+main :: IO ()
+main = do
+  html <- mapM readFile fullindex
+  commands <- runX $ readString [withParseHTML yes, withWarnings no] (mconcat html)
+    //> hasName "code"
+    //> getText
+  writeFile "all-maxima-commands.txt" (show commands)
